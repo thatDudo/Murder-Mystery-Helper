863b68d641fd27e1e9496c29108464c71c8d415c
thatDudo <someone747@gmx.net>
2021-07-07 23:39:47 +0200

    Changed config screen

--- src/main/java/com/mrqueequeg/hypixel_enhancer/gui/ScreenBuilder.java
+++ src/main/java/com/mrqueequeg/hypixel_enhancer/gui/ScreenBuilder.java
@@ -7,6 +7,8 @@ import me.shedaniel.clothconfig2.api.AbstractConfigListEntry;
 import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+import me.shedaniel.clothconfig2.gui.entries.EnumListEntry;
+import me.shedaniel.clothconfig2.impl.builders.SubCategoryBuilder;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.TranslatableText;
@@ -36,48 +38,58 @@ public class ScreenBuilder {
                 .setSaveConsumer(HypixelEnhancer::setModEnabled)
                 .build();
 
-        // innocent mode
-        AbstractConfigListEntry<Boolean> toggleMurderMysteryInnocentMode = entryBuilder.startBooleanToggle(new TranslatableText("config.generic.hypixel.murder_mystery.innocent_mode.title"), config.murdermystery.innocentHelp)
-                .setDefaultValue(defaults.murdermystery.innocentHelp)
-                .setTooltip(new TranslatableText("config.generic.hypixel.murder_mystery.innocent_mode.tooltip"))
-                .setSaveConsumer(config.murdermystery::setInnocentHelp)
-                .build();
-
-        // murder mode
-        AbstractConfigListEntry<Boolean> toggleMurderMysteryMurderMode = entryBuilder.startBooleanToggle(new TranslatableText("config.generic.hypixel.murder_mystery.murder_mode.title"), config.murdermystery.murderHelp)
-                .setDefaultValue(defaults.murdermystery.murderHelp)
-                .setTooltip(new TranslatableText("config.generic.hypixel.murder_mystery.murder_mode.tooltip"))
-                .setSaveConsumer(config.murdermystery::setMurderHelp)
-                .build();
-
-        // show name tags
-        AbstractConfigListEntry<Boolean> toggleMurderMysteryShowNameTag = entryBuilder.startBooleanToggle(new TranslatableText("config.generic.hypixel.murder_mystery.show_name_tags.title"), config.murdermystery.showNameTags)
-                .setDefaultValue(defaults.murdermystery.showNameTags)
-                .setTooltip(new TranslatableText("config.generic.hypixel.murder_mystery.show_name_tags.tooltip"))
-                .setSaveConsumer(config.murdermystery::setShowNameTags)
+        // highlight innocents
+        EnumListEntry<Config.MurderMystery.InnocentHighlightOptions> enumMurderMysteryInnocentHighlightOptions = entryBuilder.startEnumSelector(new TranslatableText("config.generic.hypixel.murder_mystery.highlight.innocent.title"), Config.MurderMystery.InnocentHighlightOptions.class, config.murdermystery.innocentHighlightOptions)
+                .setDefaultValue(defaults.murdermystery.innocentHighlightOptions)
+//                .setTooltip(new TranslatableText(""))
+                .setEnumNameProvider((e) -> ((Config.MurderMystery.InnocentHighlightOptions)e).getText())
+                .setSaveConsumer(config.murdermystery::setInnocentHighlightOptions)
                 .build();
 
         // highlight detectives
-        AbstractConfigListEntry<Boolean> toggleHighlightDetectives = entryBuilder.startBooleanToggle(new TranslatableText("config.generic.hypixel.murder_mystery.highlight_detective.title"), config.murdermystery.highlightDetectives)
-                .setDefaultValue(defaults.murdermystery.highlightDetectives)
-                .setTooltip(new TranslatableText("config.generic.hypixel.murder_mystery.highlight_detective.tooltip"))
-                .setSaveConsumer(config.murdermystery::setHighlightDetectives)
+        EnumListEntry<Config.MurderMystery.DetectiveHighlightOptions> enumMurderMysteryDetectiveHighlightOptions = entryBuilder.startEnumSelector(new TranslatableText("config.generic.hypixel.murder_mystery.highlight.detective.title"), Config.MurderMystery.DetectiveHighlightOptions.class, config.murdermystery.detectiveHighlightOptions)
+                .setDefaultValue(defaults.murdermystery.detectiveHighlightOptions)
+//                .setTooltip(new TranslatableText(""))
+                .setEnumNameProvider((e) -> ((Config.MurderMystery.DetectiveHighlightOptions)e).getText())
+                .setSaveConsumer(config.murdermystery::setDetectiveHighlightOptions)
+                .build();
+
+        // highlight murders
+        AbstractConfigListEntry<Boolean> toggleMurderMysteryHighlightMurders = entryBuilder.startBooleanToggle(new TranslatableText("config.generic.hypixel.murder_mystery.highlight.murder.title"), config.murdermystery.highlightMurders)
+                .setDefaultValue(defaults.murdermystery.highlightMurders)
+                .setTooltip(new TranslatableText("config.generic.hypixel.murder_mystery.highlight.murder.tooltip"))
+                .setSaveConsumer(config.murdermystery::setHighlightMurders)
                 .build();
 
         // highlight items
-        AbstractConfigListEntry<Boolean> toggleHighlightItems = entryBuilder.startBooleanToggle(new TranslatableText("config.generic.hypixel.murder_mystery.highlight_items.title"), config.murdermystery.highlightItems)
-                .setDefaultValue(defaults.murdermystery.highlightItems)
-                .setTooltip(new TranslatableText("config.generic.hypixel.murder_mystery.highlight_items.tooltip"))
-                .setSaveConsumer(config.murdermystery::setHighlightItems)
+        AbstractConfigListEntry<Boolean> toggleHighlightGold = entryBuilder.startBooleanToggle(new TranslatableText("config.generic.hypixel.murder_mystery.highlight.gold.title"), config.murdermystery.highlightGold)
+                .setDefaultValue(defaults.murdermystery.highlightGold)
+                .setTooltip(new TranslatableText("config.generic.hypixel.murder_mystery.highlight.gold.tooltip"))
+                .setSaveConsumer(config.murdermystery::setHighlightGold)
+                .build();
+
+        // show name tags
+        AbstractConfigListEntry<Boolean> toggleMurderMysteryShowNameTags = entryBuilder.startBooleanToggle(new TranslatableText("config.generic.hypixel.murder_mystery.misc.show_name_tags.title"), config.murdermystery.showNameTags)
+                .setDefaultValue(defaults.murdermystery.showNameTags)
+                .setTooltip(new TranslatableText("config.generic.hypixel.murder_mystery.misc.show_name_tags.tooltip"))
+                .setSaveConsumer(config.murdermystery::setShowNameTags)
                 .build();
 
 
+        SubCategoryBuilder subCatHighlight = entryBuilder.startSubCategory(new TranslatableText("config.generic.hypixel.murder_mystery.highlight.title"));
+        subCatHighlight.add(enumMurderMysteryInnocentHighlightOptions);
+        subCatHighlight.add(enumMurderMysteryDetectiveHighlightOptions);
+        subCatHighlight.add(toggleMurderMysteryHighlightMurders);
+        subCatHighlight.add(toggleHighlightGold);
+        subCatHighlight.setExpanded(true);
+
+        SubCategoryBuilder subCatMisc = entryBuilder.startSubCategory(new TranslatableText("config.generic.hypixel.murder_mystery.misc.title"));
+        subCatMisc.add(toggleMurderMysteryShowNameTags);
+        subCatMisc.setExpanded(false);
+
         catGeneric.addEntry(toggleEnabled);
-        catGeneric.addEntry(toggleMurderMysteryInnocentMode);
-        catGeneric.addEntry(toggleMurderMysteryMurderMode);
-        catGeneric.addEntry(toggleMurderMysteryShowNameTag);
-        catGeneric.addEntry(toggleHighlightDetectives);
-        catGeneric.addEntry(toggleHighlightItems);
+        catGeneric.addEntry(subCatHighlight.build());
+        catGeneric.addEntry(subCatMisc.build());
 
         return builder.build();
     }
